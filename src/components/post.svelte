abb2cdff276eedb42346b56cd7b78df90c41bbd2
Michael Lazarski <lampewebdevelopment@gmail.com>
2019-08-05 20:55:13 +0300

    added basic styling for the post page and added Josefin Sans as the font for the post page

--- src/components/post.svelte
+++ src/components/post.svelte
@@ -1,20 +1,21 @@
 <script>
     export let post;
 </script>
-<style>
-</style>
-
 <svelte:head>
     <title>{post.metedata.title}</title>
     <link rel="stylesheet" href="/monokai.css">
 </svelte:head>
 
 <div class="blog-post">
-    <header>
-        <h1> {post.metedata.title} </h1>
-        <span class="createdAt">{post.metedata.createdAt}</span>
-    </header>
-    <article>
-        {@html post.html}
-    </article>
+    <div class="columns is-mobile is-centered">
+        <div class="column is-full-mobile is-three-quarters-tablet">
+            <header>
+                <h1> {post.metedata.title} </h1>
+                <span class="createdAt">{post.metedata.createdAt}</span>
+            </header>
+            <article>
+                {@html post.html}
+            </article>
+        </div>
+    </div>
 </div>
