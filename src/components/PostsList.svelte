ca0ea2bee5dfb814c2dd83e3e6123da5b0dcf3a7
Michael Lazarski <lampewebdevelopment@gmail.com>
2019-08-14 21:19:12 +0300

    move the post list into its own componenent and implement it everywhere we are using it

---
+++ src/components/PostsList.svelte
@@ -0,0 +1,57 @@
+<script>
+    import TagsList from "../components/Tagslist.svelte";
+    export let metadata;
+</script>
+
+<style>
+    .title {
+        font-size: 2rem;
+        font-weight: 300;
+        line-height: 1.125;
+    }
+
+    .button-wrapper {
+        margin: 1.5rem 0;
+    }
+
+    .section {
+        padding-bottom: 0;
+    }
+
+    .main-page {
+        padding-top: 3rem;
+    }
+
+    hr {
+        border: 0;
+        display: block;
+        height: 0px;
+        border-top: #363636 solid 1px;
+    }
+</style>
+
+<div class="columns is-mobile is-centered">
+    <div class="column is-full-mobile is-three-quarters-tablet is-half-desktop">
+        {#each metadata as post}
+            <section class="section">
+                    <div class="header-content">
+                        <div class="has-text-centered">
+                            <h1 class="title">{post.title}</h1>
+                        </div>
+                        <p class="subheader-content has-text-centered">
+                            Series: {post.series}
+                            
+                        </p>
+                        <hr>
+                        <div class="content has-text-centered">
+                            {post.description}
+                        </div>
+                    </div>
+                    <div class="button-wrapper has-text-centered">
+                        <a rel='prefetch' class="button is-primary" href='/post/{post.slug}'>Continue Reading!</a>
+                    </div>
+                    Tags: <TagsList tags={post.tags} />
+            </section>	
+		{/each}			
+    </div>
+</div>
--- src/routes/tag/[slug].svelte
+++ src/routes/tag/[slug].svelte
@@ -13,9 +13,10 @@
 </script>
 
 <script>
-	import TagsList from "../../components/Tagslist.svelte";
+	import PostList from "../../components/PostsList.svelte";
 	export let metadata;
 </script>
+
 <style>
 	.main-page {
 		padding-top: 3rem;
@@ -23,37 +24,9 @@
 </style>
 
 <div class="main-page">
-    <div class="columns is-mobile is-centered">
-        <div class="column is-full-mobile is-three-quarters-tablet is-half-desktop">
-			{#await metadata}
-				<progress class="progress is-medium is-dark" max="100">45%</progress>
-			{:then value}
-			<div class="columns is-mobile is-centered">
-				<div class="column is-full-mobile is-three-quarters-tablet is-half-desktop">
-					{#each value as post}
-							<section class="section">
-									<div class="header-content">
-										<div class="has-text-centered">
-											<h1 class="title">{post.title}</h1>
-										</div>
-										<p class="subheader-content has-text-centered">
-											Series: {post.series}
-											
-										</p>
-										<hr>
-										<div class="content has-text-centered">
-											{post.description}
-										</div>
-									</div>
-									<div class="button-wrapper has-text-centered">
-										<a rel='prefetch' class="button is-primary" href='/post/{post.slug}'>Continue Reading!</a>
-									</div>
-									Tags: <TagsList tags={post.tags} />
-							</section>	
-							{/each}			
-						</div>
-					</div>
-			{/await}
-        </div>
-    </div>
+	{#await metadata}
+		<progress class="progress is-medium is-dark" max="100">45%</progress>
+	{:then value}
+		<PostList metadata={value} />
+	{/await}
 </div>
